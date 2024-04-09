5c6f966a8208a6f21a9f1c329350264816ecc1ad
Jan Klisowski <jan.franciszek.klisowski@gmail.com>
2024-04-09 18:12:05 +0200

    imageprocessing

---
+++ ImageProcessing/ImageProcessing/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ImageProcessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            openImageButton = new Button();
+            applyFiltersButton = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            openFileDialog1 = new OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            SuspendLayout();
+            // 
+            // openImageButton
+            // 
+            openImageButton.Location = new Point(641, 226);
+            openImageButton.Name = "openImageButton";
+            openImageButton.Size = new Size(172, 160);
+            openImageButton.TabIndex = 0;
+            openImageButton.Text = "Load";
+            openImageButton.UseVisualStyleBackColor = true;
+            openImageButton.Click += OpenImageButton_Click;
+            // 
+            // applyFiltersButton
+            // 
+            applyFiltersButton.Location = new Point(641, 408);
+            applyFiltersButton.Name = "applyFiltersButton";
+            applyFiltersButton.Size = new Size(172, 160);
+            applyFiltersButton.TabIndex = 1;
+            applyFiltersButton.Text = "Zastosuj";
+            applyFiltersButton.UseVisualStyleBackColor = true;
+            applyFiltersButton.Click += ApplyFiltersButton_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(25, 30);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(602, 726);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(863, 42);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(290, 282);
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(1186, 42);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(290, 282);
+            pictureBox3.TabIndex = 4;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(863, 445);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(290, 282);
+            pictureBox4.TabIndex = 5;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(1186, 445);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(290, 282);
+            pictureBox5.TabIndex = 6;
+            pictureBox5.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1522, 848);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(applyFiltersButton);
+            Controls.Add(openImageButton);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button openImageButton;
+        private Button applyFiltersButton;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private OpenFileDialog openFileDialog1;
+    }
+}
---
+++ ImageProcessing/ImageProcessing/Form1.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageProcessing
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap img;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenImageButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string file = openFileDialog1.FileName;
+            if (!string.IsNullOrEmpty(file))
+            {
+                img = new Bitmap(file);
+                pictureBox1.Image = img;
+            }
+        }
+
+        private void ApplyFiltersButton_Click(object sender, EventArgs e)
+        {
+            if (img == null)
+            {
+                MessageBox.Show("Please load an image first.");
+                return;
+            }
+
+            Bitmap[] processedImages = new Bitmap[4];
+
+            // Apply filters sequentially
+            processedImages[0] = ApplyGrayscale(new Bitmap(img));
+            processedImages[1] = ApplyThreshold(new Bitmap(img));
+            processedImages[2] = ApplyNegative(new Bitmap(img));
+            processedImages[3] = ApplyMirror(new Bitmap(img));
+
+            // Display processed images
+            pictureBox2.Image = processedImages[0];
+            pictureBox3.Image = processedImages[1];
+            pictureBox4.Image = processedImages[2];
+            pictureBox5.Image = processedImages[3];
+        }
+
+        private Bitmap ApplyGrayscale(Bitmap image)
+        {
+            Bitmap processedImage = new Bitmap(image.Width, image.Height);
+
+            for (int y = 0; y < processedImage.Height; y++)
+            {
+                for (int x = 0; x < processedImage.Width; x++)
+                {
+                    Color originalColor = image.GetPixel(x, y);
+                    int grayValue = (int)(originalColor.R * 0.3 + originalColor.G * 0.59 + originalColor.B * 0.11);
+                    Color grayColor = Color.FromArgb(grayValue, grayValue, grayValue);
+                    processedImage.SetPixel(x, y, grayColor);
+                }
+            }
+
+            return processedImage;
+        }
+
+        private Bitmap ApplyThreshold(Bitmap image)
+        {
+            Bitmap processedImage = new Bitmap(image.Width, image.Height);
+
+            for (int y = 0; y < processedImage.Height; y++)
+            {
+                for (int x = 0; x < processedImage.Width; x++)
+                {
+                    Color originalColor = image.GetPixel(x, y);
+                    int grayValue = (int)(originalColor.R * 0.3 + originalColor.G * 0.59 + originalColor.B * 0.11);
+                    Color thresholdColor = grayValue < 128 ? Color.Black : Color.White;
+                    processedImage.SetPixel(x, y, thresholdColor);
+                }
+            }
+
+            return processedImage;
+        }
+
+        private Bitmap ApplyNegative(Bitmap image)
+        {
+            Bitmap processedImage = new Bitmap(image.Width, image.Height);
+
+            for (int y = 0; y < processedImage.Height; y++)
+            {
+                for (int x = 0; x < processedImage.Width; x++)
+                {
+                    Color originalColor = image.GetPixel(x, y);
+                    Color negativeColor = Color.FromArgb(255 - originalColor.R, 255 - originalColor.G, 255 - originalColor.B);
+                    processedImage.SetPixel(x, y, negativeColor);
+                }
+            }
+
+            return processedImage;
+        }
+
+        private Bitmap ApplyMirror(Bitmap image)
+        {
+            Bitmap processedImage = new Bitmap(image.Width, image.Height);
+
+            for (int y = 0; y < processedImage.Height; y++)
+            {
+                for (int x = 0; x < processedImage.Width / 2; x++)
+                {
+                    Color originalColor = image.GetPixel(x, y);
+                    processedImage.SetPixel(x, y, originalColor);
+                    processedImage.SetPixel(processedImage.Width - 1 - x, y, originalColor);
+                }
+            }
+
+            return processedImage;
+        }
+    }
+}
